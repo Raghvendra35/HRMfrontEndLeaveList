1c556b68e7113d0da108a8d14b23b62fa2e6ed4c
Raghvendra35 <roms3546@gmail.com>
2023-03-27 03:14:41 -0700

    Raghvendra Routing home to Admin

--- src/app/Admin/home-first/home-first-routing.module.ts
+++ src/app/Admin/home-first/home-first-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from '../admin.component';
 import { HomeFirstComponent } from './home-first.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeFirstComponent
+    path: 'admin',
+    component: AdminComponent
   },
 
 ];
